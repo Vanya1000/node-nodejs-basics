17170bb34baa54826a253c0bb191720b67e8ebd3
Vanya1000 <Vanya1000@yandex.ru>
2022-11-29 13:24:46 +0300

    feat: implement fs task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files');
+    const destination = join(__dirname, 'files_copy');
+    try {
+        await mkdir(destination);
+        const files = await readdir(source);
+
+        for (const file of files) {
+            const sourceFile = join(source, file);
+            const destinationFile = join(destination, file);
+            await copyFile(sourceFile, destinationFile);
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files', 'wrongFilename.txt');
+    const destination = join(__dirname, 'files', 'properFilename.md');
+    try {
+        await fs.rename(source, destination);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
