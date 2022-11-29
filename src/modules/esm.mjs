bc1662ec08c6f422a5ee9fcdb84cd6ca3264756f
Vanya1000 <Vanya1000@yandex.ru>
2022-11-29 15:30:20 +0300

    feat: implement modules

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, { dirname } from "node:path";
+import { release, version } from 'node:os';
+import { fileURLToPath } from "url";
+import { createServer as createServerHttp } from "node:http";
+import('./files/c.js');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', { assert: { type: 'json' } });
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', { assert: { type: 'json' } });
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +38,5 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export { unknownObject, myServer };
 
