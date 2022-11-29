3605aa8ed238b47fe76d54300628fa92a91e717c
Vanya1000 <Vanya1000@yandex.ru>
2022-11-29 14:41:36 +0300

    feat: implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import {argv, stdout} from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const filterArgs = argv.slice(2).reduce((acc, arg, index, arr) => {
+        if (arg.startsWith('--')) {
+            const value = arr[index + 1];
+            if (value) {
+                acc.push(`${arg.slice(2)} is ${value}`);
+            }
+        }
+        return acc;
+    }, []);
+    stdout.write(filterArgs.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env, stdout } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnv = Object.entries(env).reduce((acc, [key, value]) => {
+        if (key.startsWith('RSS_')) {
+            acc.push(`${key}=${value}`);
+        }
+        return acc;
+    }, []);
+    stdout.write(rssEnv.join('; '));
 };
 
 parseEnv();
