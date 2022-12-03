5f1220057f778e9011c9ef1bbdaa511d554160b2
Vanya1000 <Vanya1000@yandex.ru>
2022-12-03 22:02:16 +0300

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { fork } from 'child_process';
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(join(__dirname, 'files', 'script.js'), args.split(' '));
 };
 
-spawnChildProcess();
+spawnChildProcess('--some --args');
